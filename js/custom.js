200a43f2c9633c867ef8030032ae9decbff5d7fe
paikhyunsuh-hash <paik.hyunsuh@gmail.com>
2025-12-09 14:14:15 +0900

    asdsaD

--- js/custom.js
+++ js/custom.js
@@ -10,3 +10,21 @@ $(function () {
     $(".projectOverview li").eq(i).stop().animate({ right: "0%" }, 300);
   });
 });
+
+/*VISUAL FADE*/
+$(function () {
+  let total = $(".preview li").length;
+  console.log(total);
+
+  let i = 0;
+
+  setInterval(function () {
+    if (i == total - 1) {
+      i = 0;
+    } else {
+      i++;
+    }
+    $(".preview li").fadeOut();
+    $(".preview li").eq(i).stop().fadeIn();
+  }, 3000);
+});
