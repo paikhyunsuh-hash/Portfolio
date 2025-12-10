46098f6457b97f9bf5037bfb31ddc7b8673566f9
paikhyunsuh-hash <paik.hyunsuh@gmail.com>
2025-12-10 14:04:37 +0900

    adsasd

--- js/custom.js
+++ js/custom.js
@@ -10,3 +10,30 @@ $(function () {
     $(".projectOverview li").eq(i).stop().animate({ right: "0%" }, 300);
   });
 });
+// GSAP Horizontal Scroll Setup
+const container = document.querySelector("#projectsPage2"); // The section element
+const slider = document.querySelector(".projectList2"); // The ul that will be translated
+
+// We need to calculate the actual width needed for the horizontal scroll.
+// This is best done on the `onRefresh` or `onResize` of ScrollTrigger
+// to account for dynamic changes and correct initial load.
+
+gsap.to(slider, {
+  // The 'x' animation value will be set dynamically via the function
+  x: () => -(slider.scrollWidth - container.offsetWidth),
+  ease: "none",
+
+  scrollTrigger: {
+    trigger: container,
+    pin: true,
+    start: "top top",
+    // Dynamically calculate the vertical scroll distance needed.
+    // The vertical scroll distance must be equal to the horizontal distance to travel.
+    end: () => "+=" + (slider.scrollWidth - container.offsetWidth),
+    scrub: 1,
+    // markers: true, // Uncomment for debugging
+
+    // Ensure the end value is re-calculated on resize/load
+    invalidateOnRefresh: true,
+  },
+});
